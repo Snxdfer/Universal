a64d33c1e78768d3e37fc0f5e3539f52ddc60e75
00Fazee <121313122+Snxdfer@users.noreply.github.com>
2025-12-19 20:40:14 -0300

    Update source.lua

--- Open-Aimbot/source.lua
+++ Open-Aimbot/source.lua
@@ -2336,7 +2336,7 @@ local OnTeleport; OnTeleport = Player.OnTeleport:Connect(function()
     if DEBUG or not Fluent or not getfenv().queue_on_teleport then
         OnTeleport:Disconnect()
     else
-        getfenv().queue_on_teleport("getfenv().loadstring(game:HttpGet(\"https\", true))()")
+        getfenv().queue_on_teleport("getfenv().loadstring(game:HttpGet(\"https://raw.githubusercontent.com/Snxdfer/Universal/refs/heads/main/Open-Aimbot/source.lua\", true))()")
         OnTeleport:Disconnect()
     end
 end)
