eeccd3f9ba3c0f41d3e572d1a163d096ac4aad5d
00Fazee <121313122+Snxdfer@users.noreply.github.com>
2026-08-21 21:17:45 -0400

    Update example-usage.lua

--- Utility/Obfuscator/example-usage.lua
+++ Utility/Obfuscator/example-usage.lua
@@ -1,1 +1,14 @@
--- nothing
+local obfuscate = loadstring(game:HttpGet("https://raw.githubusercontent.com/Snxdfer/Universal/refs/heads/main/Utility/Obfuscator/obfuscator-src.lua"))()
+local watermark = "your watermark here ez"
+local varPrefix = "pref1x_"
+
+-- your script goes inside of these things [[ ]]
+local script = [[
+
+print("wassup world")
+local bitch = 10
+print(bitch * 2)
+
+]]
+
+obfuscate(script, varPrefix, watermark)
